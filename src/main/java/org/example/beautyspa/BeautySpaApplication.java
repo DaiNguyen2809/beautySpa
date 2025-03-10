78c3815eac372f62379878f5923c801a27b50d93
Nguyễn Quốc Đại <95465798+PangasiusBrain@users.noreply.github.com>
2025-03-10 08:18:27 +0700

    create project

---
+++ src/main/java/org/example/beautyspa/BeautySpaApplication.java
@@ -0,0 +1,13 @@
+package org.example.beautyspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautySpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/beautyspa/BeautySpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.beautyspa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
