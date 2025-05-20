d88856b7f70c8e4516d37823ddbdd4401e65115b
Nguyễn Quốc Đại <nhochiprow13@gmail.com>
2025-05-20 08:31:01 +0700

    done

--- src/main/java/org/example/beautyspa/BeautySpaApplication.java
+++ src/main/java/org/example/beautyspa/BeautySpaApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeautySpaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BeautySpaApplication.class, args);
     }
-
+    // file nay de chay ung dung khong them gi nua het
 }
